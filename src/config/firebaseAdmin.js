fd2437fbd8f47e57f86d5665a3d11d143ed2d7ff
FaRaZ-32729 <sial32729@gmail.com>
2026-06-09 17:32:01 +0500

    firebase configuration

---
+++ src/config/firebaseAdmin.js
@@ -0,0 +1,20 @@
+// src/config/firebase.js
+const admin = require('firebase-admin');
+require("dotenv").config();
+
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), 
+};
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+});
+
+const bucket = admin.storage().bucket();
+
+console.log("✅ Firebase Admin Initialized");
+
+module.exports = { bucket, admin };
