5b35da17f954ce231bf2c70480265aa5f54090c6
FaRaZ-32729 <sial32729@gmail.com>
2026-06-15 20:31:58 +0500

    dafa ho ja ab namuna

--- src/routers/eventRoutes.js
+++ src/routers/eventRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const authenticate = require("../middlewares/auth");
 const checkManagePermission = require("../middlewares/checkPermission");
-const { createSchedule, manualToggle, getEventsByDevice, toggleScheduleStatus, deleteSchedule } = require("../controllers/eventController");
+const { createSchedule, manualToggle, getEventsByDevice, toggleScheduleStatus, deleteSchedule, getCurrentOrNextScheduleData } = require("../controllers/eventController");
 
 
 router.post("/create", authenticate, checkManagePermission(), createSchedule);
@@ -11,6 +11,8 @@ router.post("/create", authenticate, checkManagePermission(), createSchedule);
 router.post("/manual-toggle", authenticate, manualToggle);
 // here you have to use deviceId to get events of a device 
 router.get("/get/:deviceId", getEventsByDevice);
+// currnet or next event
+router.get("/current-next/:deviceId", authenticate, getCurrentOrNextSchedule);
 // here you have to use _id of the event
 router.patch("/:id/status", toggleScheduleStatus);
 // here you have to use _id of the event
